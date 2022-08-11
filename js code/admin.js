5a5816dfa143b1a12980eaf7bba721e963d74be0
Knight Khode <mohammedelmir594@gmail.com>
2022-08-11 19:25:38 +0000

    new additions

--- js code/admin.js
+++ js code/admin.js
@@ -4,6 +4,7 @@ const mainNav = document.querySelector('.header-nav')
 const menuBtn = document.querySelector('.menu')
 const header = document.querySelector('header')
 const closebtn = document.querySelector('.close')
+const body = document .querySelector('body')
 
 mainNav.addEventListener('mouseover',(e)=>{
     subNav.style.display="block"
@@ -16,7 +17,13 @@ menuBtn.addEventListener('click',(e)=>{
 })
 
 closebtn.addEventListener('click',(e)=>{
-    header.style.display="none"
-    menuBtn.style.display="block"
-    closebtn.style.display="none"
+    console.log(typeof(window.innerHeight))
+    if(window.innerWidth <= 1300 ){
+        header.style.display="none"
+        menuBtn.style.display="block"
+        closebtn.style.display="none"
+    }else{
+        menuBtn.style.display="none"
+        closebtn.style.display="none"  
+    }
 })
